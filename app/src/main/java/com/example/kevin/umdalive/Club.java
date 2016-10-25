8ac205ef9050a2fad94574c89b04c942cb0fd565
Ray Cerney <cerne004@d.umn.edu>
2016-10-25 08:53:32 -0500

    Added the Club class with constructor.
    Added the Club class in the .com.example.kevin.umdalive folder. The
    constructor contains clubname, username description, keyword, and post
    in the form of strings for simplified testing.

---
+++ app/src/main/java/com/example/kevin/umdalive/Club.java
@@ -0,0 +1,15 @@
+package com.example.kevin.umdalive;
+
+/**
+ * Created by user on 10/25/2016.
+ */
+
+public class Club {
+    public Club() {
+        String clubName = "DummyClub";
+        String userName ="Dummy";
+        String keyWords = "#Dummy";
+        String description = "This is a dummy.";
+        String post = "This is a dummy post";
+    }
+}
