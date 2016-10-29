86bfb908ce6dfb659ff602297c07c447ed651c2b
Luke Anderson <and03846@d.umn.edu>
2016-10-28 23:22:45 -0500

    Added club activity UI(basic dummy purposes) and code to send data about club to server NOTE: SERVER IP AND DATA MUST BE UPDATED - Luke

--- app/src/main/java/com/example/kevin/umdalive/Club.java
+++ app/src/main/java/com/example/kevin/umdalive/Club.java
@@ -1,15 +1,196 @@
 package com.example.kevin.umdalive;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by user on 10/25/2016.
  */
 
-public class Club {
-    public Club() {
-        String clubName = "DummyClub";
-        String userName ="Dummy";
-        String keyWords = "#Dummy";
-        String description = "This is a dummy.";
-        String post = "This is a dummy post";
+public class Club extends AppCompatActivity {
+
+    String clubName = "DummyClub";
+    String userName ="Dummy";
+    String keyWords = "#Dummy";
+    String description = "This is a dummy.";
+    String post = "This is a dummy post";
+
+    public Club(){
     }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_club_activity);
+
+    }
+    public void onClickMakeClub(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        restPOST(view);
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    protected void onResume() { //brings activity back to main screen.
+    super.onResume();
+    }
+
+
+    protected void onStop() {
+    super.onStop();
+    }
+
+
+    protected void onDestroy() {
+    super.onDestroy();
+    }
+
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    }
+
+
+    protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    }
+
+private class HTTPAsyncTask extends AsyncTask<String, Integer, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        HttpURLConnection serverConnection = null;
+        InputStream is = null;
+
+        Log.d("Debug:", "Attempting to connect to: " + params[0]);
+
+        try {
+            URL url = new URL( params[0] );
+            serverConnection = (HttpURLConnection) url.openConnection();
+            serverConnection.setRequestMethod(params[1]);
+            if (params[1].equals("POST") ||
+                    params[1].equals("PUT") ||
+                    params[1].equals("DELETE")) {
+                Log.d("DEBUG POST/PUT/DELETE:", "In post: params[0]=" + params[0] + ", params[1]=" + params[1] + ", params[2]=" + params[2]);
+                serverConnection.setDoOutput(true);
+                serverConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+                // params[2] contains the JSON String to send, make sure we send the
+                // content length to be the json string length
+                serverConnection.setRequestProperty("Content-Length", "" +
+                        Integer.toString(params[2].toString().getBytes().length));
+
+                // Send POST data that was provided.
+                DataOutputStream out = new DataOutputStream(serverConnection.getOutputStream());
+                out.writeBytes(params[2].toString());
+                out.flush();
+                out.close();
+            }
+
+            int responseCode = serverConnection.getResponseCode();
+            Log.d("Debug:", "\nSending " + params[1] + " request to URL : " + params[0]);
+            Log.d("Debug: ", "Response Code : " + responseCode);
+
+            is = serverConnection.getInputStream();
+
+            if (params[1] == "GET" || params[1] == "POST" || params[1] == "PUT" || params[1] == "DELETE") {
+                StringBuilder sb = new StringBuilder();
+                String line;
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                try {
+                    JSONObject jsonData = new JSONObject(sb.toString());
+                    return jsonData.toString();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            serverConnection.disconnect();
+        }
+
+        return "Should not get to this if the data has been sent/received correctly!";
+    }
+
+    /**
+     *
+     * @param result the result from the query
+     */
+    protected void onPostExecute(String result) {
+        Toast.makeText(getApplicationContext(),"Data attempting update", Toast.LENGTH_LONG).show();
+    }
+}
+    public void restPOST(View view) {
+        JSONObject jsonParam = null;
+        try {
+            //Create JSONObject here
+            jsonParam = new JSONObject();
+            jsonParam.put("clubname",clubName);
+            jsonParam.put("username", userName );
+            jsonParam.put("keywords", keyWords);
+            jsonParam.put("description", description);
+            jsonParam.put("post",post);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG:", jsonParam.toString());
+        new HTTPAsyncTask().execute("http://10.0.2.2:4321/userData", "POST", jsonParam.toString());
+    }
+
+    /**
+     * Acts as the onClick callback for the REST PUT Button. The code will generate a REST PUT
+     * action to the REST Server.
+     *
+     * @param view
+     */
+    public void restPUT(View view) {
+
+        JSONObject jsonParam = null;
+        try {
+            //Create JSONObject here
+            jsonParam = new JSONObject();
+            jsonParam.put("clubname",clubName);
+            jsonParam.put("username", userName );
+            jsonParam.put("keywords", keyWords);
+            jsonParam.put("description", description);
+            jsonParam.put("post",post);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG [PUT]:", jsonParam.toString());
+        new HTTPAsyncTask().execute("http://10.0.2.2:4321/userData", "PUT", jsonParam.toString()); //Makes sure data is sent to server
+    }
+
 }
