7eb2d34a44ed6268996296b6314ff28176a7f841
Brandon Harshe <brandonharshe@dyn-131-212-8-125.d.umn.edu>
2016-11-02 15:16:25 -0500

    Adding documentation and moving some lines around

--- umdAliveServer/index.js
+++ umdAliveServer/index.js
@@ -1,33 +1,34 @@
 
 var express = require('express');
-
-var app = express();
-
-
-var express = require('express');
 var bodyParser = require('body-parser');
 
+// Initialize main instanced class
 var app = express();
+
+// Set the port
 app.set("port",5000);
 
+// Support encoded bodies
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+// Support JSON-encoded bodies
 app.use(bodyParser.json());
 
-var clubs = new Array();//new empty array for clubs to reside
+// Empty array for clubs to reside in
+var clubs = new Array();
 
+/*
+ ************************
+ * ROUTE SECTION
+ ************************
+ */
 
-//-----------------------------
-//PUT paths
-//-----------------------------
-
-app.put('/newClub',function(req,res){
+app.put('/newClub', function(req, res) {
         console.log("At start of newClub ");
 
-        // If for some reason, the JSON isn't parsed, return a HTTP ERROR
-        // 400
+        // If for some reason the JSON isn't parsed, return HTTP error 400
         if (!req.body) return res.sendStatus(400);
 
         //takes data from request and makes into new 'club' object
@@ -37,6 +38,9 @@ app.put('/newClub',function(req,res){
                     keywords: req.body.keywords,
                     description: req.body.description,
                     post: req.body.post};
+
+
+				var club
 
         clubs[clubs.length] = club; //adds new club to array of clubs
 
@@ -55,4 +59,3 @@ app.listen(app.get("port"), function(){
     console.log('CS4531 UMDAlive app listening on port: ', app.get("port"));
 
 });
-
