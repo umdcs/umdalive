17383bfc5c041c27401023b0a59cb374e9eb9db6
Kevin Schauer <schau301@d.umn.edu>
2016-11-11 17:55:56 -0600

    changed color of menu screen and changed icons/labels on menu. No functionality yet.

--- app/src/main/java/com/example/kevin/umdalive/MainActivity.java
+++ app/src/main/java/com/example/kevin/umdalive/MainActivity.java
@@ -98,9 +98,7 @@ public class MainActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        }  else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
