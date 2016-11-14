b883aed82222aebeb8af431581eb36c7eb9ae413
Luke Anderson <and03846@d.umn.edu>
2016-11-13 19:34:32 -0600

    merged with adding users

--- app/src/main/java/com/example/kevin/umdalive/MainActivity.java
+++ app/src/main/java/com/example/kevin/umdalive/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kevin.umdalive;
 
 import android.content.Intent;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -11,14 +12,32 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.example.kevin.umdalive.R.string.post;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    UserInformation local_user_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +67,11 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
     public void onClickNewClub(View view) {
         Intent intent = new Intent(this, Club.class);
         startActivity(intent);
         // display toast in long period of time
-        Toast.makeText(getApplicationContext(),"Make a New Club!", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Make a New Club!", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -111,6 +129,92 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class HTTPAsyncTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection serverConnection = null;
+            InputStream is = null;
+
+            Log.d("Debug:", "Attempting to connect to: " + params[0]);
+
+            try {
+                URL url = new URL(params[0]);
+                serverConnection = (HttpURLConnection) url.openConnection();
+                serverConnection.setRequestMethod(params[1]);
+                if (params[1].equals("POST") ||
+                        params[1].equals("PUT") ||
+                        params[1].equals("DELETE")) {
+                    Log.d("DEBUG POST/PUT/DELETE:", "In post: params[0]=" + params[0] + ", params[1]=" + params[1] + ", params[2]=" + params[2]);
+                    serverConnection.setDoOutput(true);
+                    serverConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+                    // params[2] contains the JSON String to send, make sure we send the
+                    // content length to be the json string length
+                    serverConnection.setRequestProperty("Content-Length", "" +
+                            Integer.toString(params[2].toString().getBytes().length));
+
+                    // Send POST data that was provided.
+                    DataOutputStream out = new DataOutputStream(serverConnection.getOutputStream());
+                    out.writeBytes(params[2].toString());
+                    out.flush();
+                    out.close();
+                }
+
+                int responseCode = serverConnection.getResponseCode();
+                Log.d("Debug:", "\nSending " + params[1] + " request to URL : " + params[0]);
+                Log.d("Debug: ", "Response Code : " + responseCode);
+
+                is = serverConnection.getInputStream();
+
+                if (params[1] == "GET" || params[1] == "POST" || params[1] == "PUT" || params[1] == "DELETE") {
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    try {
+                        JSONObject jsonData = new JSONObject(sb.toString());
+                        return jsonData.toString();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                serverConnection.disconnect();
+            }
+
+            return "Should not get to this if the data has been sent/received correctly!";
+        }
+
+        public void restGET() {
+            new HTTPAsyncTask().execute("http://10.0.2.2:5000/userInformation", "GET");
+        }
+        /**
+         *
+         * @param result the result from the query
+         */
+      /*  protected void onPostExecute(String result) {
+            JSONObject user_info_JSON_Object = new JSONObject(result);
+            try{
+                    local_user_info = new UserInformation()
+
+            }
+        }
+            local_user_info.setLocal_userName();
+        }
+        */
     }
 
 }
