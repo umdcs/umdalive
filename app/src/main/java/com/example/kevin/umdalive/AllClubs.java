884e161a6870bb85d1be18211b7729574926a3d3
Kevin Schauer <schau301@d.umn.edu>
2016-11-14 18:27:02 -0600

    added new activity to display all clubs no functionality has beed done yet to access it or display anything

---
+++ app/src/main/java/com/example/kevin/umdalive/AllClubs.java
@@ -0,0 +1,16 @@
+package com.example.kevin.umdalive;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kevin on 11/14/2016.
+ */
+
+public class AllClubs extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_clubs);
+    }
+}
