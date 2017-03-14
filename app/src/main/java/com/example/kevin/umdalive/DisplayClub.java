8a281cd96eac70e7de58556995b57ada5fe0d40e
Andrew Miller <mill6841@d.umn.edu>
2017-03-13 21:56:09 -0500

    Added comments to DisplayClub.java

--- app/src/main/java/com/example/kevin/umdalive/DisplayClub.java
+++ app/src/main/java/com/example/kevin/umdalive/DisplayClub.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 
 /**
  * Created by Luke on 12/6/2016.
+ * Pretty simply class. Just displays a club.
  */
 
 public class DisplayClub extends AppCompatActivity {
@@ -28,6 +29,7 @@ public class DisplayClub extends AppCompatActivity {
     private static String administrator;
     private static String keywords;
 
+    //these set methods are all called by AllClubs activity when the user selects an activity. 
     public static void setDescription(String tempDescription){
         description = tempDescription;
     }
@@ -57,6 +59,9 @@ public class DisplayClub extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_club);
+        // idk why someone decided to make these EditTexts... The user should not be able to edit this stuff.
+        // These should just be TextViews and there should be an EditText next to it that allows a user to put info there.
+        // ALSO, nothing the user puts in here saves at this point so thats something that definitely should be fixed.
         EditText clubnameEditText = (EditText) findViewById(R.id.display_club_name);
         EditText discriptionEditText = (EditText) findViewById(R.id.display_club_description);
         EditText keywordEditText = (EditText) findViewById(R.id.display_clubs_keyword);
