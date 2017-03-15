cc46cb61db22928b80c3760c552b5cb36d6401f7
Andrew Miller <mill6841@d.umn.edu>
2017-03-15 16:47:19 -0500

    Created RestModel class. Added AsyncTask stuff and constructor.

---
+++ app/src/main/java/com/example/kevin/umdalive/RestModel.java
@@ -0,0 +1,114 @@
+package com.example.kevin.umdalive;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Andrew on 3/15/2017.
+ */
+
+public class RestModel {
+    private Context context;
+
+    public RestModel(Context context){
+        this.context = context;
+    }
+
+    public void setContext(Context context){
+        this.context = context;
+    }
+
+
+    //typical AsyncTask stuff. Probably shouldnt be controlled by the view...
+    private class HTTPAsyncTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection serverConnection = null;
+            InputStream is = null;
+
+            Log.d("Debug:", "Attempting to connect to: " + params[0]);
+
+            try {
+                URL url = new URL(params[0]);
+                serverConnection = (HttpURLConnection) url.openConnection();
+                serverConnection.setRequestMethod(params[1]);
+                if (params[1].equals("POST") ||
+                        params[1].equals("PUT") ||
+                        params[1].equals("DELETE")) {
+                    Log.d("DEBUG POST/PUT/DELETE:", "In post: params[0]=" + params[0] + ", params[1]=" + params[1] + ", params[2]=" + params[2]);
+                    serverConnection.setDoOutput(true);
+                    serverConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+                    // params[2] contains the JSON String to send, make sure we send the
+                    // content length to be the json string length
+                    serverConnection.setRequestProperty("Content-Length", "" +
+                            Integer.toString(params[2].toString().getBytes().length));
+
+                    // Send POST data that was provided.
+                    DataOutputStream out = new DataOutputStream(serverConnection.getOutputStream());
+                    out.writeBytes(params[2].toString());
+                    out.flush();
+                    out.close();
+                }
+
+                int responseCode = serverConnection.getResponseCode();
+                Log.d("Debug:", "\nSending " + params[1] + " request to URL : " + params[0]);
+                Log.d("Debug: ", "Response Code : " + responseCode);
+
+                is = serverConnection.getInputStream();
+
+                if (params[1] == "GET" || params[1] == "POST" || params[1] == "PUT" || params[1] == "DELETE") {
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    try {
+                        JSONObject jsonData = new JSONObject(sb.toString());
+                        return jsonData.toString();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                serverConnection.disconnect();
+            }
+
+            return "Should not get to this if the data has been sent/received correctly!";
+        }
+
+        /**
+         * @param result the result from the query
+         */
+        protected void onPostExecute(String result) {
+            Toast.makeText(context, "Data transfer sucessful", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+}
