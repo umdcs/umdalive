272ab14823bad5332a9a706f2902c9c209379787
Ryan Gebhart <Gebha105@d.umn.edu>
2017-03-17 16:18:06 -0500

    Created ClubInformationModel and edited UserInformationModel. Biggest changes include adding a function to create JSON strings and getters/setters functions.

---
+++ app/src/main/java/com/example/kevin/umdalive/ClubInformationModel.java
@@ -0,0 +1,153 @@
+package com.example.kevin.umdalive;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by rgebh_000 on 3/17/2017.
+ *
+ * Class to define a club.
+ *
+ * Constructor requires a club name but no other data to instantiate
+ */
+
+public class ClubInformationModel {
+    private String clubName;
+    private String userName;
+    private String keyWords;
+    private String description;
+    private String post;
+
+    private Object keywordItem = new Object(); //this item grabs from user WTF is this?
+
+    /**
+     * Constructor requiring club name to create
+     *
+     * @param clubName
+     */
+    public ClubInformationModel(String clubName) {
+        this.clubName = clubName;
+        userName = "";
+        keyWords = "";
+        description = "";
+        post = "";
+    }
+
+    /**
+     * setter for clubName
+     *
+     * @param clubName string of new club
+     */
+    public void setClubName(String clubName){
+        this.clubName = clubName;
+    }
+
+    /**
+     * getter for clubName
+     *
+     * @return clubName
+     */
+    public String getClubName() {
+        return clubName;
+    }
+
+    /**
+     * setter for userName
+     *
+     * assuming this means whoever created the club
+     *
+     * @param userName String of userName
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * getter for userName
+     *
+     * @return userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * setter for keywords
+     *
+     * @param keyWords string of new keyword with '#' in front of it
+     */
+    public void setKeyWords(String keyWords){
+        this.keyWords = keyWords;
+    }
+
+    /**
+     * getter for keywords
+     *
+     * @return keywords
+     */
+    public String getKeyWords() {
+        return keyWords;
+    }
+
+    /**
+     * setter for description
+     *
+     * @param description new string for description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * getter Description
+     *
+     * @return Description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * setter for post
+     *
+     * @param post sets the new post for the club
+     */
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    /**
+     * getter for post
+     *
+     * @return post
+     */
+    public String getPost() {
+        return post;
+    }
+
+    /**
+     * Function to create a JSON object of a club
+     * JSON object is then made into a string and returned
+     *
+     * @return jsonString string form of JSON object Club
+     */
+    public String jsonStringify() {
+        JSONObject jsonString = null;
+        try {
+            //Create JSONObject here
+            jsonString = new JSONObject();
+            jsonString.put("clubname", clubName);
+            jsonString.put("username", userName);
+            jsonString.put("keywords", keyWords);
+            jsonString.put("description", description);
+            jsonString.put("post", post);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG:", jsonString.toString());
+
+        return jsonString.toString();
+    }
+}
