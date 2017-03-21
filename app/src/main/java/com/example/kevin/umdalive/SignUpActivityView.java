16e13824b86d38f6b93ce5c2a26799ee36ab422b
Wesley Osumo <osumo001@d.umn.edu>
2017-03-21 11:09:41 -0500

    some code fixes for signupactivityView

--- app/src/main/java/com/example/kevin/umdalive/SignUpActivityView.java
+++ app/src/main/java/com/example/kevin/umdalive/SignUpActivityView.java
@@ -10,23 +10,21 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
-
 public class SignUpActivityView extends AppCompatActivity {
 
 
     private Presenter presenter;
 
-    private   EditText email;
-    private EditText Password;
-    private EditText userNamee;
+    private   EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText userNameEditText;
     private Spinner gradDate;
-    private Spinner Major;
+    private Spinner majorSpinner;
 
 
 
     private String password;
-    private String Email;
+    private String email;
     private String userName;
 
     private String graduation;
@@ -58,8 +56,8 @@ public class SignUpActivityView extends AppCompatActivity {
 
 
 
-//---------------------------------------------------------------------Spinner for the Major
-      Major=(Spinner) findViewById(R.id.major_input);
+//---------------------------------------------------------------------Spinner for the majorSpinner
+      majorSpinner =(Spinner) findViewById(R.id.major_input);
 
 
         //creating the major adapter for the spinner
@@ -69,7 +67,7 @@ public class SignUpActivityView extends AppCompatActivity {
         majorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         //applying the Majoradapter to the spinner
-        Major.setAdapter(majorAdapter);
+        majorSpinner.setAdapter(majorAdapter);
 
 
     }
@@ -81,16 +79,16 @@ public class SignUpActivityView extends AppCompatActivity {
      */
     protected void signUp(View view){
 
-        //retrieving email adress from the EditText object and converting it to a string
-        email=(EditText) findViewById(R.id.new_email_input);
-        String emailText= email.getText().toString();
+        //retrieving emailEditText adress from the EditText object and converting it to a string
+        emailEditText =(EditText) findViewById(R.id.new_email_input);
+        String emailText= emailEditText.getText().toString();
         //retrieving password and converting it to string
-        Password=(EditText) findViewById(R.id.new_password_input);
-        String passwordText=Password.getText().toString();
+        passwordEditText =(EditText) findViewById(R.id.new_password_input);
+        String passwordText= passwordEditText.getText().toString();
 
         //getting username from the edit text object
-        userNamee=(EditText) findViewById(R.id.new_name_input);
-        String nameTemp=userNamee.getText().toString();
+        userNameEditText =(EditText) findViewById(R.id.new_name_input);
+        String nameTemp= userNameEditText.getText().toString();
 
 /**
  * if the emailadress entered has an '@' symbol
@@ -100,21 +98,21 @@ public class SignUpActivityView extends AppCompatActivity {
         if(emailText.indexOf('@') != -1){
           //setting the graduation date
             setGraduation(gradDate.getSelectedItem().toString());
-             setMajor(Major.getSelectedItem().toString());
-            setEmail(emailText);
-            setPassword(passwordText);
+             setMajorSpinner(majorSpinner.getSelectedItem().toString());
+            setEmailEditText(emailText);
+            setPasswordEditText(passwordText);
             setUserName(nameTemp);
 
-           System.out.println(getMajor());
-            Log.d(getMajor(),getMajor());
+           System.out.println(getMajorSpinner());
+            Log.d(getMajorSpinner(), getMajorSpinner());
             Log.d(getGraduation(),getGraduation());
 
-            presenter.setUserInfo(this.getUserName(),this.getPassword(),this.getEmail(),this.getMajor(),this.getGraduation());
+            presenter.setUserInfo(userName,password, email,major,graduation);
 
 
         }
 
-        else Toast.makeText(getApplicationContext(),"Sorry, this is not a valid email.", Toast.LENGTH_SHORT).show();
+        else Toast.makeText(getApplicationContext(),"Sorry, this is not a valid emailEditText.", Toast.LENGTH_SHORT).show();
 
 
 
@@ -128,16 +126,16 @@ public class SignUpActivityView extends AppCompatActivity {
     //setters and getters
 
     public void setGraduation(String grad){this.graduation=grad;}
-    public void setMajor(String mess){this.major=mess;}
-    public void setEmail(String mess){this.Email=mess;}
-    public void setPassword(String mess){this.password=mess;}
+    public void setMajorSpinner(String mess){this.major=mess;}
+    public void setEmailEditText(String mess){this.email =mess;}
+    public void setPasswordEditText(String mess){this.password=mess;}
     public void setUserName(String mess){this.userName=mess;}
 
 
-    public String getPassword(){return password;}
-    public String getEmail(){return Email;}
+    public String getPasswordEditText(){return password;}
+    public String getEmailEditText(){return email;}
     public String getGraduation(){return graduation;}
-    public String getMajor(){return major;}
+    public String getMajorSpinner(){return major;}
 
     public String getUserName(){return userName;}
 
