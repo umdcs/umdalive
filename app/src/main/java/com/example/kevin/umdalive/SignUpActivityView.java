175697847fc016be5c91a14b37de874febc5a8c0
Wesley Osumo <osumo001@d.umn.edu>
2017-03-20 23:06:48 -0500

    Sign-upactivty now has setters and getter for username, and can now pass on data to the presenter.

--- app/src/main/java/com/example/kevin/umdalive/SignUpActivityView.java
+++ app/src/main/java/com/example/kevin/umdalive/SignUpActivityView.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 
@@ -18,11 +19,15 @@ public class SignUpActivityView extends AppCompatActivity {
 
     private   EditText email;
     private EditText Password;
+    private EditText userNamee;
     private Spinner gradDate;
     private Spinner Major;
 
+
+
     private String password;
     private String Email;
+    private String userName;
 
     private String graduation;
     private  String major;
@@ -82,6 +87,11 @@ public class SignUpActivityView extends AppCompatActivity {
         //retrieving password and converting it to string
         Password=(EditText) findViewById(R.id.new_password_input);
         String passwordText=Password.getText().toString();
+
+        //getting username from the edit text object
+        userNamee=(EditText) findViewById(R.id.new_name_input);
+        String nameTemp=userNamee.getText().toString();
+
 /**
  * if the emailadress entered has an '@' symbol
  * then use the setter to set private datafields
@@ -93,17 +103,18 @@ public class SignUpActivityView extends AppCompatActivity {
              setMajor(Major.getSelectedItem().toString());
             setEmail(emailText);
             setPassword(passwordText);
+            setUserName(nameTemp);
 
            System.out.println(getMajor());
             Log.d(getMajor(),getMajor());
             Log.d(getGraduation(),getGraduation());
 
-
+            //presenter.setUserInfo(this.getUserName(),this.getPassword(),this.getEmail(),this.getMajor(),this.getGraduation());
 
 
         }
 
-
+        else Toast.makeText(getApplicationContext(),"Sorry, this is not a valid email.", Toast.LENGTH_SHORT).show();
 
 
 
@@ -120,12 +131,15 @@ public class SignUpActivityView extends AppCompatActivity {
     public void setMajor(String mess){this.major=mess;}
     public void setEmail(String mess){this.Email=mess;}
     public void setPassword(String mess){this.password=mess;}
+    public void setUserName(String mess){this.userName=mess;}
 
 
     public String getPassword(){return password;}
     public String getEmail(){return Email;}
     public String getGraduation(){return graduation;}
     public String getMajor(){return major;}
+
+    public String getUserName(){return userName;}
 
 
 }
