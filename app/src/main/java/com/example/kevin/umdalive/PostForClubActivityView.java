50b1dfcd05ef3d270b1708e867fb5837738315de
Andrew Miller <mill6841@d.umn.edu>
2017-03-27 21:56:12 -0500

    Fixed PostForClubActivityView so clubs are displayed when the activity is started.

--- app/src/main/java/com/example/kevin/umdalive/PostForClubActivityView.java
+++ app/src/main/java/com/example/kevin/umdalive/PostForClubActivityView.java
@@ -1,7 +1,89 @@
+
 package com.example.kevin.umdalive;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
 
-public class PostForClubActivityView extends AppCompatActivity {
+
+public class PostForClubActivityView extends Activity {
+
+    ListView listView;
+    boolean launchActivity;
+    Presenter presenter;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        presenter = new Presenter(this);
+        launchActivity = false;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_for_club_select);
+        listView = (ListView) findViewById(R.id.list);
+        Intent intent = new Intent(this, PostingActivity.class);
+        setView();
+        //launch is changed to true once a club is clicked on.
+        if(launchActivity){
+            startActivity(intent);
+        }
+    }
+
+
+    /**
+     * Sets up the view.
+     *
+     * For clarification, an ArrayAdapter is used to take the contents of clubNames and display
+     * it on a default layout provided by Android(simple_list_item_1).
+     *
+     * The listener is used to check if a club has been clicked. Once clicked, we send a get request to the server
+     * and the club info is received in the response. The response is used to set the info for DisplayClubView before it is launched.
+     * I don't like the way the previous group implemented this part, mainly because it doesn't work lol.
+     * We will have to fix it(setDisplayClubInfo(String) in the presenter).
+     */
+    private void setView() {
+        ArrayList<String> clubNames = presenter.getClubNames();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, clubNames);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                launchActivity = true;
+                int itemPosition = position;
+                String itemValue = (String) listView.getItemAtPosition(position);
+                String jsonResponse = presenter.restGet(new String("getClub"), itemValue);
+                presenter.setDisplayClubInfo(jsonResponse);
+                Toast.makeText(getApplicationContext(), "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onResume() { //brings activity back to main screen.
+        super.onResume();
+    }
+
+    protected void onStop() {
+        super.onStop();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
 }
