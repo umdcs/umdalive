f9a2a2679d283dc941babd5653a5174a7c0cde07
Jenna Anderson <and03780@d.umn.edu>
2017-03-30 11:10:38 -0500

    Deleted some comments for stuff we wont be using abymore

--- app/src/main/java/com/example/kevin/umdalive/LoginActivity.java
+++ app/src/main/java/com/example/kevin/umdalive/LoginActivity.java
@@ -1,74 +1,33 @@
 package com.example.kevin.umdalive;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 
 //import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.OptionalPendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 
 /**
  * Created by Luke on 11/7/2016.
  */
 
 public class LoginActivity extends AppCompatActivity {
-//probably will be moved to te view
+            GoogleApiClient.OnConnectionFailedListener,
+            View.OnClickListener {
 
 
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.login_activity);
+
+
+
+
+
+
+
+//    public void signUpScreen(View view){
+//        //Intent intent = new Intent(this, SignUpActivity.class);
+//        //startActivity(intent);
 //    }
-//
-    /*
-    This is the onClick for the Login button
-    current functionality: currently all this does is check for "@" symbol and go to next page.
-    The next page is the Main Activity.
-
-    Replace with OAuth
-     */
-
-    //This function will be moved to the view
-
-
-
-    /*
-    This is the onClick for the signup button of the lower right corner.
-    current functionality: All this does is bring you to a new activity
-    that will help create a new user.
-
-    This brings you to SignUpActivity
-     */
-    //stays here
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public void signUpScreen(View view){
-        //Intent intent = new Intent(this, SignUpActivity.class);
-        //startActivity(intent);
-    }
     /*
     Methods for Activity to inherit parent
      */
