05acb4c307490db025e62225d03934add0f8230d
Wesley Osumo <osumo001@d.umn.edu>
2017-04-03 18:37:18 -0500

    Google Sign in Beta 1.

--- app/src/main/java/com/example/kevin/umdalive/LoginView.java
+++ app/src/main/java/com/example/kevin/umdalive/LoginView.java
@@ -21,10 +21,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
-public class LoginView extends AppCompatActivity {
+public class LoginView extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
 
 
@@ -42,20 +43,193 @@ public class LoginView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-         presenter = new Presenter(this);
+        initializeButtons();
+        GoogleSignInitializer();
 
-        presenter.googleSignIn();
+
+
+    }
+
+    private Button signedOutButton;
+    private  SignInButton signInButton;
+    private RelativeLayout Rlayout;
+
+    private static final String TAG ="SignInActivity" ;
+    String mFullName;
+    String mEmail;
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleApiClient mGoogleApiClient;
+    private TextView mStatusTextView;
+    private ProgressDialog mProgressDialog;
+
+    public void initializeButtons(){
+        signedOutButton = (Button) findViewById(R.id.btn_sign_out);
+        signInButton= (SignInButton) findViewById(R.id.sign_in_button);
+        Rlayout=(RelativeLayout) findViewById(R.id.RLayout);
+
+
+
+        signedOutButton.setOnClickListener(this);
+        signInButton.setOnClickListener(this);
+
+
+    }
+
+
+
+    public void GoogleSignInitializer(){
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+// options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+
     }
 
 
 
 
+    public void signUp() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
 
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            mFullName=acct.getDisplayName();
+            mEmail=acct.getEmail();
+
+
+
+
+
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+    /**
+     * this method determines whether or not the sign in or sign out button is visible based on the param
+     * @param signedIn whether or not the user is signed in
+     */
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            signInButton.setVisibility(View.GONE);
+            signInButton.setVisibility(View.VISIBLE);
+        }
+
+        else {
+
+            signInButton.setVisibility(View.VISIBLE);
+            signedOutButton.setVisibility(View.GONE);
+
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+
+    // [START signIn]
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // [END signIn]
+
+    // [START signOut]
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        // [START_EXCLUDE]
+                        updateUI(false);
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END signOut]
+
+
+  
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.btn_sign_out:
+                signOut();
+                break;
+
+        }
 
 
 
 
     }
+}
+
+
+
+
+
+
+
+
+
 
 
 
