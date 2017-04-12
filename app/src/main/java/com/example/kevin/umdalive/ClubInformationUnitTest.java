c074c5a47cc5ccb794b65d14ecb0767062d79535
Jenna Anderson <and03780@d.umn.edu>
2017-04-12 17:29:54 -0500

    Still adding to the testing class, adding first tests to check if variable match

--- app/src/main/java/com/example/kevin/umdalive/ClubInformationUnitTest.java
+++ app/src/main/java/com/example/kevin/umdalive/ClubInformationUnitTest.java
@@ -1,10 +1,31 @@
 package com.example.kevin.umdalive;
 
+import android.util.Log;
+
 /**
  * Created by Jennacide on 4/12/2017.
  */
 
 public class ClubInformationUnitTest {
 
+public void TestClub1 (){
+    String clubName1="Sassy Club";
+    String username1="Sassysquatch";
+    String keywords1="Rhonda";
+    String description1="fabulous";
+    CreateClub testClub1=null;
+    testClub1.makeClub(clubName1, username1, keywords1, description1);
+    ClubInformationModel testOne = new ClubInformationModel(clubName1, username1, keywords1, description1);
+    String nameMatch1;
+    String usernameMatch1;
+    String keywordsMatch1;
+    String descriptionMatch1;
+
+    if(testOne.getClubName().toString().equals(clubName1))
+    {
+        String thing1 = "If this displays, then the club names match!";
+       // Log.d(thing1);
+    }
+}
 
 }
