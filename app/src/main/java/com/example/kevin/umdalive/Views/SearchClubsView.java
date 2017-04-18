5cb66406ffcb9a8ca541eab389b40587b1db2fe2
Ryan Gebhart <Gebha105@d.umn.edu>
2017-04-18 16:29:37 -0500

    changed some spelling

--- app/src/main/java/com/example/kevin/umdalive/Views/SearchClubsView.java
+++ app/src/main/java/com/example/kevin/umdalive/Views/SearchClubsView.java
@@ -32,20 +32,24 @@ public class SearchClubsView extends AppCompatActivity {
 
         Spinner spinner = (Spinner) findViewById(R.id.keywordChooser); // Create an ArrayAdapter using the string array and a default spinner layout
         //keyword_list is the list of all the club categories. We should probably expand this at some point and add on "other" option.
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.keyword_list, android.R.layout.simple_spinner_item); // Specify the layout to use when the list of choices appears
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+                (this, R.array.keyword_list, android.R.layout.simple_spinner_item); // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
         //dealing with the selected option of the spinner
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                 keywordItem = parent.getItemAtPosition(pos);
+
+                setView();
+
+
             }
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
 
 
-        //setView();
     }
 
     /**
