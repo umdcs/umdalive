12b6e2475eb387702c4d9e5c1785c28e19ab686b
wesley <osumo001@d.umn.edu>
2017-04-18 19:45:14 -0500

    made some progress on the userdata

---
+++ app/src/main/java/com/example/kevin/umdalive/Views/userdata.java
@@ -0,0 +1,15 @@
+package com.example.kevin.umdalive.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kevin.drawerexample.R;
+
+public class userdata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userdata);
+    }
+}
